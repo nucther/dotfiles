d020552c25690a530d2f446d26522dc58c80fec1
nurohman <me@nurohman.com>
2023-05-29 06:56:07 +0700

    update javadate

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #
 export CC=gcc-12 CXX=g++-12
 
-yay -S gcc12 hyprland xdg-desktop-portal-hyprland pipewire wireplumber jq grim slurp wlogout swaylock-effects  waybar-hyprland zsh dunst kitty tmux wl-clipboard 
+yay -S gcc12 hyprland xdg-desktop-portal-hyprland pipewire wireplumber jq grim slurp wlogout swaylock-effects  waybar-hyprland zsh dunst kitty tmux wl-clipboard fuzzel 
 
 stow hypr dunst kitty nixos nvim scripts tmux zsh waybar swaylock  
 
--- waybar/.config/waybar/scripts/javadate.sh
+++ waybar/.config/waybar/scripts/javadate.sh
@@ -1,9 +1,17 @@
 #!/bin/bash 
 
-javaMarketDateName=(Pahing Pon Wage Kliwon Legi) 
+javaMarketDateName=(Pon Wage Kliwon Legi Pahing)
 
-
-dayOfWeek=$(date +"%u")
-
-
-echo "( $(date +"%a") ${javaMarketDateName[($dayOfWeek %5)]} )"
+dateTarget="today"
+dayOfWeek=$(date --date="$dateTarget" +"%u")
+now=$(date --date="$dateTarget" +"%s")
+first=$(date --date="1938/1/1" +"%s")
+dayOfJavaneseMarket=$((($now + $first) / 86400))
+res=$((dayOfJavaneseMarket%5))
+#echo "now: $now"
+#echo "first: $first"
+#echo "x=$(($now + $first))"
+#echo "Java: $dayOfJavaneseMarket"
+#echo $(($dayOfJavaneseMarket % 5 ))
+#echo $(date --date="$dateTarget" +"%d %b %Y")
+echo "( $(date --date="$dateTarget" +"%a") ${javaMarketDateName[$res]} )"
