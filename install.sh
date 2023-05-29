c6654e87890bceaf5b8a3790e37463d56a8646ef
Nurohman <muslem27@gmail.com>
2023-05-29 17:39:07 +0000

    Update file install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@
 #
 export CC=gcc-12 CXX=g++-12
 
-yay -S gcc12 hyprland xdg-desktop-portal-hyprland pipewire wireplumber jq grim slurp wlogout swaylock-effects  waybar-hyprland zsh dunst kitty tmux wl-clipboard fuzzel stow ttf-hack-nerd zsh
+yay -S gcc12 hyprland xdg-desktop-portal-hyprland pipewire wireplumber jq grim slurp wlogout swaylock-effects  waybar-hyprland zsh dunst kitty tmux wl-clipboard fuzzel stow ttf-hack-nerd zsh xorg-xinit
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 stow hypr dunst kitty nixos nvim scripts tmux zsh waybar swaylock  
+
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
 
