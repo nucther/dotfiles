2c17be2a677024a51c1ad1d2231d3b39047d1711
nurohman <me@nurohman.com>
2023-05-31 18:31:22 +0700

    update javadate market name

--- waybar/.config/waybar/scripts/javadate.sh
+++ waybar/.config/waybar/scripts/javadate.sh
@@ -1,17 +1,11 @@
 #!/bin/bash 
 
-javaMarketDateName=(Pon Wage Kliwon Legi Pahing)
+javaMarketDateName=(Wage Kliwon Legi Pahing Pon)
 
-dateTarget="today"
+dateTarget=$(date +"%d %b %Y")
 dayOfWeek=$(date --date="$dateTarget" +"%u")
 now=$(date --date="$dateTarget" +"%s")
 first=$(date --date="1938/1/1" +"%s")
 dayOfJavaneseMarket=$((($now + $first) / 86400))
 res=$((dayOfJavaneseMarket%5))
-#echo "now: $now"
-#echo "first: $first"
-#echo "x=$(($now + $first))"
-#echo "Java: $dayOfJavaneseMarket"
-#echo $(($dayOfJavaneseMarket % 5 ))
-#echo $(date --date="$dateTarget" +"%d %b %Y")
 echo "( $(date --date="$dateTarget" +"%a") ${javaMarketDateName[$res - 1]} )"
